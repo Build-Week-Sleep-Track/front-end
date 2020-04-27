f6b0612e3dffbb80495b71c2543aab3b2a4c01c3
Your Name <you@example.com>
2020-04-27 15:02:24 -0400

    added router around app, created registration components

--- sleep-app/src/App.js
+++ sleep-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Link, useHistory } from 'react-router-dom';
+import Registration from './components/Registraton';
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
     <h1> 
       Hello world
     </h1>
+    <Registration/>
     </div>
   );
 }
---
+++ sleep-app/src/components/Registraton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Registration(){
+    return (
+        <h1>inside Registration</h1>
+    )
+}
+
+export default Registration;
