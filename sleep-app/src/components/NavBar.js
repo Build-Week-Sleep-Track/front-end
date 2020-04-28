92180a5466c78755e59e1d905457e62496e255f8
Patrick Dal Gobbo <juandalgobbo@Juans-Air.lan1>
2020-04-28 18:38:18 -0400

    form for new entries

---
+++ sleep-app/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <div>
+            
+            <Link to='/login'>Logout</Link>
+        </div>
+    );
+};
+
+export default NavBar;
--- sleep-app/src/index.js
+++ sleep-app/src/index.js
@@ -1,31 +1,23 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import Reducer from './reducers/reducer';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+const store = createStore(Reducer, applyMiddleware(thunk))
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </Provider>,
+  document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
