9ba68a0702e09a8590f4dfb0a78b000cb6e3af27
Your Name <you@example.com>
2020-04-29 20:51:27 -0400

    latest changes with merge from main

--- sleep-app/src/components/HomePage.js
+++ sleep-app/src/components/HomePage.js
@@ -1,35 +1,57 @@
 import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
-import HomeCard from './HomeCard';
+import { Route, NavLink, useHistory } from 'react-router-dom';
 
 // components
 import NavBar from "./NavBar";
+import HomeCard from './HomeCard';
 
 
 const HomePage = () => {
-  let [sleepList, setSleepList] = useState([])
+  
+const {push} = useHistory();
+const [sleepList, setSleepList] = useState([]);
 
   useEffect(() => {
+   
     axiosWithAuth()
-    .get('https://sleep-tracker2020.herokuapp.com/api/users')
+    .get('/users')
     .then(res => {
       console.log(res);
+
       setSleepList(res.data.sessions)
-      
     })
 
     .catch(err => {
       console.log(err, 'you have an error');
     })
-  }, [])
+  
+  
+}, [])
 
   return (
     <div>
       <NavBar/>
         <h1>Welcome to main page</h1>
+
+        <ul>
+          {sleepList ? sleepList.map(sleep => (
+            <li key={sleep.id}>
+            Start- {sleep.sleep_start}
+            End- {sleep.sleep_end}
+            </li>
+          ))
+        : null}
+        </ul>
+
         <HomeCard
         sessions={sleepList}
         />
+
+        <button className='nd-button' onClick={() => push('/sleep-form')}>Add Session</button>
+        
+
+
       
     </div>
   );
